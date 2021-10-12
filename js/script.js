70da82f459521b2e48682a2bdb86833c1e3cdd76
becca-b <reba.brunner@gmail.com>
2021-10-12 06:22:03 -0400

    Created async function to fetch user data.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//Profile information:
+const overview = document.querySelector(".overview");
+const username = "Becca-RB";
+
+const gitInfo = async function () {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    console.log(data);
+    displayUserInfo(data);
+};
+
+gitInfo();
+
+const displayUserInfo = async function (data){
+    const newDiv = document.createElement ("div");
+    newDiv.classList.add ("user-info");
+    newDiv.innerHTML = 
+    `<figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>`;
+
+    overview.append(newDiv);
+};
